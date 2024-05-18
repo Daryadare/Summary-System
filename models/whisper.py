0b713efafe8bbfa99ceb59f49adab6b43263910d
Daryadare <Kalmykova.darya@mail.ru>
2024-05-19 00:44:32 +0300

    Suppressing unwanted messages from the used models

--- models/whisper.py
+++ models/whisper.py
@@ -1,8 +1,9 @@
 import re
 import torch
+from tools import suppress_output
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-import warnings
-warnings.filterwarnings("ignore")
+from transformers import logging
+logging.set_verbosity_error()
 
 
 def load_model_processor():
@@ -37,21 +38,22 @@ def clean_whisper_output(res_w: str) -> tuple[str, str]:
 
 def transcribe(filepath: str):
 
-    model, processor, torch_dtype = load_model_processor()
+    with suppress_output.SuppressOutput():
+        model, processor, torch_dtype = load_model_processor()
 
-    pipe = pipeline(
-        "automatic-speech-recognition",
-        model=model,
-        tokenizer=processor.tokenizer,
-        feature_extractor=processor.feature_extractor,
-        max_new_tokens=64,
-        chunk_length_s=64,
-        batch_size=32,
-        return_timestamps=True,
-        torch_dtype=torch_dtype,
-    )
+        pipe = pipeline(
+            "automatic-speech-recognition",
+            model=model,
+            tokenizer=processor.tokenizer,
+            feature_extractor=processor.feature_extractor,
+            max_new_tokens=64,
+            chunk_length_s=64,
+            batch_size=32,
+            return_timestamps=True,
+            torch_dtype=torch_dtype,
+        )
 
-    result = pipe(filepath,
-                  generate_kwargs={"language": "russian"},
-                  return_timestamps=True)
+        result = pipe(filepath,
+                      generate_kwargs={"language": "russian"},
+                      return_timestamps=True)
     return result
