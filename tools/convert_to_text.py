cb3769d5c96b14a73124ba8552234b87d38aba89
Daryadare <Kalmykova.darya@mail.ru>
2024-05-18 18:59:15 +0300

    Updated the formation of the output file name and 2-step entry

--- tools/convert_to_text.py
+++ tools/convert_to_text.py
@@ -3,7 +3,7 @@ from datetime import date
 
 
 def create_file(exist_dir: str, outputs_to_write: list,
-                in_process_state: int):
+                in_process_state: int, curr_file: str):
 
     new_dir_name = '/lecture_resumes'
     new_dir_path = exist_dir+new_dir_name
@@ -11,20 +11,25 @@ def create_file(exist_dir: str, outputs_to_write: list,
         os.mkdir(new_dir_path)
 
     today = str(date.today())
-    new_file_name = new_dir_path+'/lecture-'+today
+    new_file_name = new_dir_path+'/lecture-'+today+'-v0'
 
     if in_process_state == 0:
+        file_vers = 1
         while os.path.exists(new_file_name):
-            file_vers = 0
-            new_file_name = new_dir_path+'/lecture-'+today+f'-v{file_vers}'
+            part_name = new_file_name[:new_file_name.find('-v')]
+            new_file_name = part_name+f'-v{file_vers}'
             file_vers += 1
         mode = 'w'
-    elif os.path.exists(new_file_name) and in_process_state > 0:
+    elif os.path.exists(curr_file) and in_process_state > 0:
         mode = 'a'
+        new_file_name = curr_file
+        outputs_to_write.append('Ответы на ваши вопросы сгенерированы нейросетью. '
+                                'Пожалуйста проверяйте важную информацию! \n')
     else:
         mode = 'w'
 
-    with open(new_file_name, mode) as file:
+    new_file_name = new_file_name + '.txt'
+    with open(new_file_name, mode, encoding='utf-8') as file:
         for el in outputs_to_write:
             file.write(el + '\n\n')
     file.close()
